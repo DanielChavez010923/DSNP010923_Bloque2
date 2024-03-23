cd688067471f5969d47d6e3c5f05027dd6e1bedb
DanielChavez010923 <163219796+DanielChavez010923@users.noreply.github.com>
2024-03-23 04:02:23 +0000

    conflictos resueltos

--- conversor.sh
+++ conversor.sh
@@ -143,25 +143,3 @@ cx=$(echo "($valor*($destino2/$origen2))" | bc -l)
 
 #printf "\n $valor $texto1 = %.2f $cx $texto2 \n"
 printf "\n $valor $texto1 = %.2f $texto2 \n" $cx
-=======
-
-echo "Conversor de unidades"
-echo "1 milimetros a centimetros"
-echo "2 centimetros a Metros"
-echo "3 Metros a decametros"
-echo "4 decametros a Hectometros"
-echo "5 Hectometros a Kilometros"
-
-read -p "ingrese su opcion: " opcion
-
-#Metro = 1
-#centimetro=$((Metro/10))
-
-if [ opcion -eq 1 ]
-   
-   then
-   read -p "Ingrese el valor a convertir" valor1
-   
-
-fi
->>>>>>> 4d3ee289498128ab3b472d21226b8355542cd388
